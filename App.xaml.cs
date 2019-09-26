15f068b7d9dc4262c96cd2f4ee5c7303b8e2b8a0
yzh549345916 <549345916@qq.com>
2019-09-26 16:46:37 +0800

    更改整体框架为telerik

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,10 @@ namespace _2019HSQXSJK
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            new MainWindow().Show();
+            base.OnStartup(e);
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,18 +4,28 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Threading;
+using Telerik.Windows.Controls;
 
 namespace _2019HSQXSJK
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : RadWindow
     {
         private NotifyIcon _notifyIcon = null;
         public MainWindow()
         {
+
+            LocalizationManager.Manager = new LocalizationManager()
+            {
+                ResourceManager = GridViewResources.ResourceManager
+            };
             InitializeComponent();
+            Settheme settheme1 = new Settheme();
+            StyleManager.SetTheme(this, GetMyTheme("Crystal"));
+            settheme1.setTheme(settheme1.setLightOrDark("Crystal"));
+            StyleManager.ApplicationTheme = GetMyTheme("Crystal");
 
             //rkcs();
 
@@ -50,10 +60,10 @@ namespace _2019HSQXSJK
         /// 重载OnClosing()函数实现--使用户无法通过点击右上角的关闭按钮来关闭窗口
         /// </summary>
         /// <param name="e"></param>
-        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
-        {
-            e.Cancel = true;
-        }
+        //protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        //{
+        //    e.Cancel = true;
+        //}
 
         private void T1_TextChanged_1(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
@@ -97,6 +107,83 @@ namespace _2019HSQXSJK
             {
                 cs();
             });
+        }
+        public Theme GetMyTheme(string name)
+        {
+            string myName = name.ToLower();
+            if (myName.Contains("crystal"))
+            {
+                return new CrystalTheme();
+            }
+            else if (myName.Contains("fluent"))
+            {
+                return new FluentTheme();
+            }
+            else if (myName.Contains("material"))
+            {
+                return new MaterialTheme();
+            }
+            else if (myName.Contains("office2016touch"))
+            {
+                return new Office2016TouchTheme();
+            }
+            else if (myName.Contains("office2016"))
+            {
+                return new Office2016Theme();
+            }
+            else if (myName.Contains("green"))
+            {
+                return new GreenTheme();
+            }
+            else if (myName.Contains("office2013"))
+            {
+                return new Office2013Theme();
+            }
+            else if (myName.Contains("visualstudio2013"))
+            {
+                return new VisualStudio2013Theme();
+            }
+            else if (myName.Contains("windows8touch"))
+            {
+                return new Windows8TouchTheme();
+            }
+            else if (myName.Contains("windows8"))
+            {
+                return new Windows8Theme();
+            }
+            else if (myName.Contains("office_black"))
+            {
+                return new Office_BlackTheme();
+            }
+            else if (myName.Contains("office_blue"))
+            {
+                return new Office_BlueTheme();
+            }
+            else if (myName.Contains("office_silver"))
+            {
+                return new Office_SilverTheme();
+            }
+            else if (myName.Contains("summer"))
+            {
+                return new SummerTheme();
+            }
+            else if (myName.Contains("vista"))
+            {
+                return new VistaTheme();
+            }
+            else if (myName.Contains("transparent"))
+            {
+                return new TransparentTheme();
+            }
+            else if (myName.Contains("windows7"))
+            {
+                return new Windows7Theme();
+            }
+            else if (myName.Contains("expression_dark"))
+            {
+                return new Expression_DarkTheme();
+            }
+            return new CrystalTheme();
         }
     }
 }
